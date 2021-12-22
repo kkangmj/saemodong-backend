7f8878c53bcde93e9d38cd1a083b865d84411d52
minju <bbak0402dd@gmail.com>
2021-12-22 17:30:22 +0900

    feat: Add notification entity and repository

--- src/main/java/com/saemodong/api/model/notification/Notification.java
+++ src/main/java/com/saemodong/api/model/notification/Notification.java
@@ -1,2 +1,49 @@
-package com.saemodong.api.model.notification;public class Notification {
+package com.saemodong.api.model.notification;
+
+import com.saemodong.api.model.BaseTimeEntity;
+import com.saemodong.api.model.user.User;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 1, nullable = false)
+  @ColumnDefault("'N'")
+  private String isRead;
+
+  @Column private LocalDateTime readAt;
+
+  @Column(length = 1, nullable = false)
+  @ColumnDefault("'N'")
+  private String isSent;
+
+  @Column private LocalDateTime sentAt;
+
+  @Column private Integer type;
+
+  @Column private String title;
+
+  @Column private String content;
+
+  @ManyToOne
+  @JoinColumn(name = "userId")
+  private User user;
 }
--- src/main/java/com/saemodong/api/repository/NotificationRepository.java
+++ src/main/java/com/saemodong/api/repository/NotificationRepository.java
@@ -1,2 +1,11 @@
-package com.saemodong.api.repository;public interface NotificationRepository {
+package com.saemodong.api.repository;
+
+import com.saemodong.api.model.notification.Notification;
+import com.saemodong.api.model.user.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+  Optional<Notification> findByUserOrderByIsSentDesc(User user);
 }
