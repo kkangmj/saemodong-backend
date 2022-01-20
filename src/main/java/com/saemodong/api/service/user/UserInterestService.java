72351f4ff1f39c913f1ac60e49edf68125a7c44c
minju <bbak0402dd@gmail.com>
2022-01-20 15:36:15 +0900

    refactor: Service 구체 클래스의 인터페이스 정의
    - controller가 service 구체 클래스에 의존하지 않도록 변경. controller는 고수준 모듈, service는 저수준 모듈. 고수준 모듈이 저수준 모듈에 직접적으로 의존하지 않도록 service를 추상화함.

--- src/main/java/com/saemodong/api/service/user/UserInterestService.java
+++ src/main/java/com/saemodong/api/service/user/UserInterestService.java
@@ -1,105 +1,17 @@
 package com.saemodong.api.service.user;
 
-import com.saemodong.api.common.KeyHelper;
 import com.saemodong.api.dto.user.ContestInterestDto;
 import com.saemodong.api.dto.user.ExtraInterestDto;
-import com.saemodong.api.model.user.User;
-import com.saemodong.api.repository.user.UserRepository;
-import com.saemodong.api.service.user.interest.ContestFieldService;
-import com.saemodong.api.service.user.interest.ContestOrganizerService;
-import com.saemodong.api.service.user.interest.ContestPrizeService;
-import com.saemodong.api.service.user.interest.ContestTypeService;
-import com.saemodong.api.service.user.interest.ExtraDistrictService;
-import com.saemodong.api.service.user.interest.ExtraFieldService;
-import com.saemodong.api.service.user.interest.ExtraOrganizerService;
-import com.saemodong.api.service.user.interest.ExtraTypeService;
-import javax.transaction.Transactional;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-@Service
-public class UserInterestService {
+public interface UserInterestService {
+  ExtraInterestDto getUserExtraInterest(String apiKey);
 
-  private final ExtraTypeService extraTypeService;
-  private final ExtraFieldService extraFieldService;
-  private final ExtraOrganizerService extraOrganizerService;
-  private final ExtraDistrictService extraDistrictService;
-  private final ContestTypeService contestTypeService;
-  private final ContestFieldService contestFieldService;
-  private final ContestOrganizerService contestOrganizerService;
-  private final ContestPrizeService contestPrizeService;
+  ContestInterestDto getUserContestInterest(String apiKey);
 
-  private final UserRepository userRepository;
-  private final KeyHelper keyHelper;
+  void setUserExtraInterest(
+      String apiKey, String type, String field, String organizer, String district);
 
-  public ExtraInterestDto getUserExtraInterest(String apiKey) {
+  void setUserContestInterest(String apiKey, String type, String field, String organizer, String prize);
 
-    User user = keyHelper.getUser(apiKey);
 
-    String extraTypeStr = extraTypeService.getConditionStr(user);
-    String extraFieldStr = extraFieldService.getConditionStr(user);
-    String extraOrganizerStr = extraOrganizerService.getConditionStr(user);
-    String extraDistrictStr = extraDistrictService.getConditionStr(user);
-
-    return ExtraInterestDto.of(extraTypeStr, extraFieldStr, extraOrganizerStr, extraDistrictStr);
-  }
-
-  public ContestInterestDto getUserContestInterest(String apiKey) {
-
-    User user = keyHelper.getUser(apiKey);
-
-    String contestTypeStr = contestTypeService.getConditionStr(user);
-    String contestFieldStr = contestFieldService.getConditionStr(user);
-    String contestOrganizerStr = contestOrganizerService.getConditionStr(user);
-    String contestPrizeStr = contestPrizeService.getConditionStr(user);
-
-    return ContestInterestDto.of(
-        contestTypeStr, contestFieldStr, contestOrganizerStr, contestPrizeStr);
-  }
-
-  public void setUserExtraInterest(
-      String apiKey, String type, String field, String organizer, String district) {
-
-    User user = keyHelper.getUser(apiKey);
-
-    extraTypeService.setCondition(user, type);
-    extraFieldService.setCondition(user, field);
-    extraOrganizerService.setCondition(user, organizer);
-    extraDistrictService.setCondition(user, district);
-
-    updateSetInterest(user);
-  }
-
-  public void setUserContestInterest(
-      String apiKey, String type, String field, String organizer, String prize) {
-
-    User user = keyHelper.getUser(apiKey);
-
-    contestTypeService.setCondition(user, type);
-    contestFieldService.setCondition(user, field);
-    contestOrganizerService.setCondition(user, organizer);
-    contestPrizeService.setCondition(user, prize);
-
-    updateSetInterest(user);
-  }
-
-  @Transactional
-  public void updateSetInterest(User user) {
-
-    if (extraTypeService.existsByUser(user)
-        || extraFieldService.existsByUser(user)
-        || extraOrganizerService.existsByUser(user)
-        || extraDistrictService.existsByUser(user)
-        || contestTypeService.existsByUser(user)
-        || contestFieldService.existsByUser(user)
-        || contestOrganizerService.existsByUser(user)
-        || contestPrizeService.existsByUser(user)) {
-      user.updateSetInterest("Y");
-    } else {
-      user.updateSetInterest("N");
-    }
-    userRepository.save(user);
-  }
 }
--- src/main/java/com/saemodong/api/service/user/UserService.java
+++ src/main/java/com/saemodong/api/service/user/UserService.java
@@ -1,40 +1,10 @@
 package com.saemodong.api.service.user;
 
-import com.saemodong.api.common.KeyHelper;
 import com.saemodong.api.dto.user.UserLoginDto;
 import com.saemodong.api.dto.user.UserRegisterDto;
-import com.saemodong.api.exception.NicknameDuplicateException;
-import com.saemodong.api.exception.UserNotFoundException;
-import com.saemodong.api.model.user.User;
-import com.saemodong.api.repository.user.UserRepository;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-@Service
-public class UserService {
+public interface UserService {
+  UserRegisterDto registerByNickname(String nickname);
 
-  private final UserRepository userRepository;
-  private final KeyHelper keyHelper;
-
-  public UserRegisterDto registerByNickname(String nickname) {
-
-    if (userRepository.existsByNickname(nickname)) {
-      throw new NicknameDuplicateException(nickname);
-    }
-
-    String apiKey = keyHelper.getApiKey();
-    userRepository.save(User.of(nickname, apiKey));
-
-    return UserRegisterDto.of(nickname, apiKey, "N");
-  }
-
-  public UserLoginDto loginByApiKey(String apiKey) {
-
-    User user =
-        userRepository.findByApiKey(apiKey).orElseThrow(() -> new UserNotFoundException(apiKey));
-
-    return UserLoginDto.of(user.getNickname(), user.getSetInterest());
-  }
+  UserLoginDto loginByApiKey(String apiKey);
 }
